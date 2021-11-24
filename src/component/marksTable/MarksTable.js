59e70ae033af3192daeb6f7955773ca8a5cf5fe0
KVirB <dlaydrygaakk@mail.ru>
2021-11-24 22:57:46 +0300

    hardcode

--- src/component/marksTable/MarksTable.js
+++ src/component/marksTable/MarksTable.js
@@ -105,7 +105,136 @@ tabTitles.unshift("ФИО");
 console.log(tabTitles);
 //const rows = students.map ()
 export default class MarksTable extends React.Component {
-  state = {};
+  state = [
+    {
+        id: 1,
+        discipline: {
+            id: 1,
+            name: "Автоматизация информационного обеспечения",
+            idFromSource: 3
+        },
+        teacher: {
+            id: 1,
+            surname: "Абазовская",
+            name: "Наталья",
+            patronymic: "Викторовна",
+            idFromSource: 3
+        },
+        group: {
+            id: 1,
+            name: "Дзк-19"
+        },
+        streamId: null,
+        journalHeaders: [
+            {
+                id: 1,
+                subGroup: 1,
+                classTopic: null,
+                discription: null,
+                dateOfLesson: "15.12.2021",
+                hoursCount: null,
+                typeClass: {
+                    id: 1,
+                    name: "Лабораторная работа"
+                },
+                journalContents: [
+                    {
+                        id: 1,
+                        presence: true,
+                        grade: 5,
+                        discription: null,
+                        student: {
+                            id: 1,
+                            surname: "Седова",
+                            name: "Мария",
+                            patronymic: "Петровна",
+                            subGroup: 1
+                    }
+                    },
+                    {	
+                        id: 2,
+                        presence: false,
+                        grade: 3,
+                        discription: null,
+                          student: {
+                          id: 2,
+                          surname: "Липская",
+                          name: "Анна",
+                          patronymic: "Григорьевна",
+                          subGroup: 1
+                    }
+                    },
+                    {
+                        id: 3,
+                        presence: true,
+                        grade: 7,
+                        discription: null,
+                        student: {
+                            id: 3,
+                            surname: "Цепото",
+                            name: "Виктор",
+                            patronymic: "Михайлович",
+                            subGroup: 1
+                    }
+                    }
+                ]
+            },
+            {
+              id: 2,
+              subGroup: 1,
+              classTopic: null,
+              discription: null,
+              dateOfLesson: "21.11.2021",
+              hoursCount: null,
+              typeClass: {
+                  id: 1,
+                  name: "Лабораторная работа"
+              },
+              journalContents: [
+                  {
+                      id: 1,
+                      presence: true,
+                      grade: 8,
+                      discription: null,
+                      student: {
+                          id: 1,
+                          surname: "Седова",
+                          name: "Мария",
+                          patronymic: "Петровна",
+                          subGroup: 1
+                  }
+                  },
+                  {	
+                      id: 2,
+                      presence: false,
+                      grade: 6,
+                      discription: null,
+                        student: {
+                        id: 2,
+                        surname: "Липская",
+                        name: "Анна",
+                        patronymic: "Григорьевна",
+                        subGroup: 1
+                  }
+                  },
+                  {
+                      id: 3,
+                      presence: true,
+                      grade: 10,
+                      discription: null,
+                      student: {
+                          id: 3,
+                          surname: "Цепото",
+                          name: "Виктор",
+                          patronymic: "Михайлович",
+                          subGroup: 1
+                  }
+                  }
+              ]
+          }
+        ]
+    }
+]
 
   componentDidMount() {
     console.log(this.props.journalsite + "ureureorieorie");
@@ -120,134 +249,127 @@ export default class MarksTable extends React.Component {
       return <div key={item.id}>{item.dateOfLesson}</div>;
     });
   }
-  //   render() {
-  //     return (
-  //       <div>
-  //         {/* <Header /> */}
-  //         <TableContainer sx={{ maxHeight: 440 }}>
-  //           <Table
-  //             stickyHeader
-  //             aria-label="sticky table"
-  //             style={{ maxWidth: 0, minWidth: 0 }}
-  //           >
-  //             <TableHead>
-  //               <TableRow>
-  //                 {/* {tabTitles.map((cl, i) => (
-  //                   <TableCell className="line-cell line-up" key={i}>
-  //                     <div className="vertical cellwidth">{cl}</div>
-  //                   </TableCell>
-  //                 ))} */}
-  //                 <TableCell className="line-cell line-up">
-  //                   <div>ФИО</div>
-  //                 </TableCell>
-  //                 {/* {this.getData().map((cl, i) => (
-  //                   <TableCell className="line-cell line-up" key={i}>
-  //                     <div className="vertical cellwidth">{cl}</div>
-  //                   </TableCell>
-  //                 ))} */}
-  //                 {this.props.dataLesson.map((m) => (
-  //                   <TableCell className="line-cell line-up">
-  //                     <div className="vertical cellwidth">{m.dateOfLesson}</div>
-  //                   </TableCell>
-  //                 ))}
-  //               </TableRow>
-  //             </TableHead>
-  //             <TableBody>
-  //               {students.map((student, i) => {
-  //                 return (
-  //                   <TableRow key={i}>
-  //                     <TableCell className="line-cell" key={-1}>
-  //                       {student.fio}
-  //                     </TableCell>
-  //                     {student.marks.map((mark, i) => (
-  //                       <TableCell className="line-cell" key={i}>
-  //                         <div className="cellwidth">
-  //                           <input
-  //                             className="myInput"
-  //                             type="text"
-  //                             // defaultValue={mark.grade}
-  //                             onBlur={(e) => console.log(e.target.value)}
-  //                             onFocus={(e) => e.target.select()}
-  //                           />
-  //                         </div>
-  //                       </TableCell>
-  //                     ))}
-  //                   </TableRow>
-  //                 );
-  //               })}
-  //             </TableBody>
-  //           </Table>
-  //         </TableContainer>
-  //         {/* journalHeaders.journalContents.student */}
-  //         {this.props.journalsite.map((m) => (
-  //           <div>{m.id}</div>
-  //         ))}
-  //         {this.props.dataLesson.map((m) => (
-  //           <div>{m.dateOfLesson}</div>
-  //         ))}
-  //       </div>
-  //     );
-  //   }
-  // }
-  render() {
-    return (
-      <div>
-        <TableContainer sx={{ maxHeight: 440 }}>
-          <Table
-            stickyHeader
-            aria-label="sticky table"
-            style={{ maxWidth: 0, minWidth: 0 }}
-          >
-            <TableHead>
-              <TableRow>
-                <TableCell className="line-cell line-up">
-                  <div>ФИО</div>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {students.map((student, i) => {
-                return (
-                  <TableRow key={i}>
-                    <TableCell className="line-cell" key={-1}>
-                      {student.fio}
+    render() {
+      return (
+        <div>
+          {/* <Header /> */}
+          <TableContainer sx={{ maxHeight: 440 }}>
+            <Table
+              stickyHeader
+              aria-label="sticky table"
+              style={{ maxWidth: 0, minWidth: 0 }}
+            >
+              <TableHead>
+                <TableRow>
+                  {/* {tabTitles.map((cl, i) => (
+                    <TableCell className="line-cell line-up" key={i}>
+                      <div className="vertical cellwidth">{cl}</div>
                     </TableCell>
-                    {student.marks.map((mark, i) => (
-                      <TableCell className="line-cell" key={i}>
-                        <div className="cellwidth">
-                          <input
-                            className="myInput"
-                            type="text"
-                            defaultValue={mark.grade}
-                            onBlur={(e) => console.log(e.target.value)}
-                            onFocus={(e) => e.target.select()}
-                          />
-                        </div>
+                  ))} */}
+                  <TableCell className="line-cell line-up">
+                    <div>ФИО</div>
+                  </TableCell>
+                  {/* {this.getData().map((cl, i) => (
+                    <TableCell className="line-cell line-up" key={i}>
+                      <div className="vertical cellwidth">{cl}</div>
+                    </TableCell>
+                  ))} */}
+                  {this.props.dataLesson.map((m) => (
+                    <TableCell className="line-cell line-up">
+                      <div className="vertical cellwidth">{m.dateOfLesson}</div>
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {students.map((student, i) => {
+                  return (
+                    <TableRow key={i}>
+                      <TableCell className="line-cell" key={-1}>
+                        {student.fio}
                       </TableCell>
-                    ))}
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        {/* journalHeaders.journalContents.student */}
-        {this.props.journalsite.map(
-          (m) =>
-            m.journalHeaders.map((item) => {
-              return <div>{item.dateOfLesson}</div>;
-            })
-          // forEach((item) => {
-          //   <div>{item.dateOfLesson}</div>;
-          // })
-        )}
-        {/* {this.props.dataLesson.map((m) => (
-          <div>{m.dateOfLesson}</div>
-        ))} */}
-      </div>
-    );
+                      {student.marks.map((mark, i) => (
+                        <TableCell className="line-cell" key={i}>
+                          <div className="cellwidth">
+                            <input
+                              className="myInput"
+                              type="text"
+                              defaultValue={mark.grade}
+                              onBlur={(e) => console.log(e.target.value)}
+                              onFocus={(e) => e.target.select()}
+                            />
+                          </div>
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          {/* journalHeaders.journalContents.student */}
+          {this.props.journalsite.map((m) => (
+            <div>{m.id}</div>
+          ))}
+          {this.props.dataLesson.map((m) => (
+            <div>{m.dateOfLesson}</div>
+          ))}
+        </div>
+      );
+    }
   }
-}
+//   render() {
+//     return (
+//       <div>
+//         <TableContainer sx={{ maxHeight: 440 }}>
+//           <Table
+//             stickyHeader
+//             aria-label="sticky table"
+//             style={{ maxWidth: 0, minWidth: 0 }}
+//           >
+//                 <TableCell className="line-cell line-up">
+//                   <div>ФИО</div>
+//                 </TableCell>
+              
+//                 {/* {this.state.map((m) => 
+//                   m.journalHeaders.map((item) => {
+//                     return(
+//                       <TableCell className="line-cell line-up">
+//                         <div className="vertical cellwidth">{item.dateOfLesson}</div>
+//                       </TableCell>
+//                     )
+//                   })
+//               )} */}
+//               { this.state.map((m) => 
+//                    m.journalHeaders.map((item) => {
+//                 return  item.journalContents.map((content,i) => {
+//                     return(
+//                       <TableRow>
+//                         <TableCell className="line-cell">
+//                           {content.student.name}
+//                         </TableCell>
+//                         <TableCell className="line-cell">
+//                           <div className="cellwidth">
+//                             <input
+//                               className="myInput"
+//                               type="text"
+//                               defaultValue={content.grade}
+//                               onBlur={(e) => console.log(e.target.value)}
+//                               onFocus={(e) => e.target.select()}
+//                             />
+//                           </div>
+//                         </TableCell>
+//                       </TableRow>
+//                     )
+//                   })
+//                   })
+//               )}
+//           </Table>
+//         </TableContainer>
+//       </div>
+//     );
+//   }
+// }
 
 // {
 //   props.marks.map((m) => (
