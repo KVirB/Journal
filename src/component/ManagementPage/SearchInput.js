e1e85f189b68b2de13563797b613363d3af78935
KVirB <dlaydrygaakk@mail.ru>
2023-04-04 16:59:53 +0300

    fixId

--- src/component/ManagementPage/SearchInput.js
+++ src/component/ManagementPage/SearchInput.js
@@ -11,10 +11,13 @@ const SearchInput = (props) => {
     <>
       <ReactSearchAutocomplete
         items={props.teachersSearch.map((item) => ({
-          id: item.idFromSource,
+          id: item.id,
           name: item.surname + " " + item.name + " " + item.patronymic,
+          idFromSource: item.idFromSource,
         }))}
-        onSelect={(log) => props.getTeacherManagementByIdThunk(log.id)}
+        onSelect={(log) =>
+          props.getTeacherManagementByIdThunk(log.idFromSource)
+        }
         onClear={props.getTeacherManagement}
         // onSearch={props.teachers.length === 1 ? props.getTeacherManagement : ""}
         // onSearch={props.getTeacherManagement}
